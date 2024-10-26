7d354fe30160162d0c3a6b7c902b475adb10c437
Jack Ricketts <95173538+JLR24@users.noreply.github.com>
2024-10-26 20:49:01 +0100

    Implement data frames.

--- website/datafunc/query.py
+++ website/datafunc/query.py
@@ -4,7 +4,7 @@ import pandas as pd
 def getMatch(oppostion, dataframe):
     return (dataframe.query('opposition_team == @opposition'))
 
-def getMatch(matchId, dataframe):
+def getMatch(match_id, dataframe):
     return (dataframe.query('match_id == @match_id'))
 
 def getGoalsScoredBySeason(dataframe):
--- website/results/results.py
+++ website/results/results.py
@@ -1,9 +1,20 @@
 from flask import Blueprint, render_template, request
+from ..datafunc.query import getMatch
+from ..datafunc.preparation import clean_dataframe, create_dataframe
+import pandas as pd
+
 
 results = Blueprint("results", __name__, template_folder="results_templates", static_folder="results_static")
+frame: pd.DataFrame = clean_dataframe(create_dataframe("assets/dataset.csv"))
 
-@results.route("/match")
-def match_page():
+def _get_match(id):
+    for i in frame.index:
+        if float(frame["match_id"][i]) == float(id):
+            return i
+
+@results.route("/match/<string:id>")
+def match_page(id):
+    # Get match from ID and pass in to template.
     tab = request.args.get("tab") or "summary"
-    print(tab)
-    return render_template("match.html", tab=tab)
+    print(frame["Opposition"][_get_match(id)])
+    return render_template("match.html", tab=tab, i=_get_match(id), frame=frame)
