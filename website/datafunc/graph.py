a8a9c80d91823db0912a08da11094b3da4435db3
Sadiyya Nailarani <Naila.Nailarani@warwick.ac.uk>
2024-10-26 17:49:35 +0100

    Edit graph function

--- website/datafunc/graph.py
+++ website/datafunc/graph.py
@@ -3,22 +3,19 @@ import matplotlib as plt
 import preparation as prep
 import query as query
 
-def main():
-    df = prep.create_dataframe('assets\dataset.csv')
-    prep.clean_dataframe(df)
-    makeGraph(df)
-
-
-if __name__ == "__main__":
-    main()
 
 
 def makeGraph(dataframe):
     goalsBySeason = query.getGoalsScoredBySeason(dataframe)
-    # print(goalsBySeason.groupby(['season_name']))
+    #print(goalsBySeason.groupby(['season_name']))
     print(goalsBySeason)
 
 
 
+def main():
+    df = prep.create_dataframe('assets/dataset.csv')
+    prep.clean_dataframe(df)
+    makeGraph(df)
 
-    
+if __name__ == "__main__":
+    main()
