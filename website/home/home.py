ffd6102b378b9ba2a91ff8c6224ebfdb5f71bc52
Jack Ricketts <95173538+JLR24@users.noreply.github.com>
2024-10-26 11:34:54 +0100

    Fix template bugs.

--- website/home/home.py
+++ website/home/home.py
@@ -1,4 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 home = Blueprint("home", __name__, template_folder="home_templates", static_folder="home_static")
 
+@home.route("/")
+def index():
+    return render_template("index.html", data=["this", "is", "data"])
