e2ff7a4b40e2142fe3bc27a985722c89929d6721
anjaleep15 <anjalee_patel@hotmail.com>
2024-10-26 16:19:00 +0100

    Prepare website for match outcomes

--- website/home/home.py
+++ website/home/home.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, request
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
 home = Blueprint("home", __name__, template_folder="home_templates", static_folder="home_static")
 
@@ -9,10 +11,34 @@ def index():
 @home.route("/search")
 def search():
     query = request.args.get("query")
+    matches = []
     search_results = []
+    filter = True
+
     if query: # If the user has entered a search query...
-        search_results = [] # ...load the matching matches to display.
+        matches = [] # ...load the matching matches to display.
+        for match in matches:
+            if fuzz.ratio(query, match.opponent_name) >= 90:
+                location = request.args.get("location")
+                season = request.args.get("season")
+                result = request.args.get("result")
+
+                if location != None:
+                    if location != match.location:
+                        filter = False
+                if season != None:
+                    if season != match.season_name:
+                        filter = False
+                if result != None:
+                    if result != match.match_outcome:
+                        filter = False
+
+                if filter == True:                                
+                    search_results.append(match)
+                
+    
     
     # <WIP: apply filters (home/away, season, win/loss)>
 
     return render_template("search.html", results=search_results, query=query)
+
