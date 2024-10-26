1a5f8353bdb846942cfc1c4dfd78a0897b953698
ascomlexicon <git.zone075@8alias.com>
2024-10-26 16:02:38 +0100

    Clean DataFrame contents and columns

--- data-functions/preparation.py
+++ data-functions/preparation.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from dataconstants import IGNORE_COLUMNS
 
@@ -10,4 +11,15 @@ def create_dataframe(filepath):
 
 def clean_dataframe(dataframe):
     clean_frame = dataframe.dropna()
+    
+    clean_frame["match_outcome"] = np.where(
+        clean_frame["match_outcome"].str.contains("lost"), clean_frame["match_outcome"].str.replace("lost", "L"), np.where(
+            clean_frame["match_outcome"].str.contains("won"), clean_frame["match_outcome"].str.replace("won", "W"), clean_frame["match_outcome"].str.replace("draw", "D")   
+        ) 
+    )
+    
+    clean_frame["location"] = np.where(
+        clean_frame["location"].str.contains("away"), clean_frame["location"].str.replace("away", "A"), clean_frame["location"].str.replace("home", "H")
+    )
+
     return clean_frame.reset_index(drop=True)
